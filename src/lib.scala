852d9ffc7f15514969c2bf303c68714fa5e31e32
Samuel Shan <samuel.shan@me.com>
2023-05-30 21:17:12 +0800

    project: use scala-cli to build project

---
+++ src/lib.scala
@@ -0,0 +1,1 @@
+// This is a placeholder file, for Idea to identify root of source folder
---
+++ test/EzScalaTest.scala
@@ -0,0 +1,19 @@
+//> using toolkit latest
+
+import com.wiwiwa.ezscala.*
+
+class EzScalaTest extends munit.FunSuite:
+    test("PipeOperator"){
+        val list = List(9,10,11)
+        assertEquals( list|Integer.toHexString, List("9","a","b") )
+        assertEquals( list|?{_<10}, List(9) )
+        assertEquals( list||{e=>List(e,e*e)}, List(9,81,10,100,11,121) )
+    }
+    test("Url"){
+        assert{
+            "./.gitignore".file
+              |! { "Checking content of file " + _ |> println }
+              |> { _.text.contains(".*") }
+        }
+        assert( "https://www.gov.cn/".url.text.contains("政府") )
+    }
