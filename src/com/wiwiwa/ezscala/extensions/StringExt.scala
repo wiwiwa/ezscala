f8d01c1b8eb90b0987810d459ac7c56603336a84
Samuel Shan <samuel.shan@me.com>
2024-03-10 10:02:26 +0800

    String: add `base64`, `base64Url`

--- src/com/wiwiwa/ezscala/extensions/StringExt.scala
+++ src/com/wiwiwa/ezscala/extensions/StringExt.scala
@@ -1,9 +1,12 @@
 package com.wiwiwa.ezscala.extensions
 
 import java.nio.charset.StandardCharsets
+import java.util.Base64
 
 trait StringExt:
     extension (string:String)
         def bytes = string.getBytes(StandardCharsets.UTF_8)
     extension (buf:Array[Byte])
         def string = new String(buf,StandardCharsets.UTF_8)
+        def base64 = Base64.getEncoder.encodeToString(buf)
+        def base64Url = Base64.getUrlEncoder.encodeToString(buf)
