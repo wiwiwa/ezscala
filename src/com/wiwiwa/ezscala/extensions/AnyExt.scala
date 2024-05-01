75c9c25c36b90fe0b743701b65b033fe325142d0
Samuel Shan <samuel.shan@me.com>
2024-05-01 16:51:38 +0800

    `&&`: always return type of right value

--- src/com/wiwiwa/ezscala/extensions/AnyExt.scala
+++ src/com/wiwiwa/ezscala/extensions/AnyExt.scala
@@ -5,8 +5,14 @@ import com.wiwiwa.ezscala.extensions.AnyExt.*
 trait AnyExt:
   extension[T] (value:T)
     /** if `value` is truthy, return `truthyValue`, else return null */
-    inline def `&&`[V](truthyValue: =>V): T|V =
-      if isTruthy(value) then truthyValue else value
+    inline def `&&`[V](truthyValue: =>V): V =
+      if isTruthy(value) then truthyValue else inline null.asInstanceOf[T] match
+        case v: V => value.asInstanceOf[V]
+        case _ => inline null.asInstanceOf[V] match
+          case _: Boolean => false.asInstanceOf[V]
+          case _: Int | _: Long => 0.asInstanceOf[V]
+          case _: Float => 0.asInstanceOf[V]
+          case _ => null.asInstanceOf[V]
     /** if `value` is truthy, return `value`, else return rightValue */
     inline def `||`[V](rightValue: =>V): T|V =
       if isTruthy(value) then value else rightValue
